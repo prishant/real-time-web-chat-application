77c3a057cf9291e7c4f710bf3fbfccacfb54356c
Rishant Pal <77202422+prishant@users.noreply.github.com>
2022-01-22 14:37:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const http=require("http");
+const express=require("express");
+
+const app=express();
+
+const server=http.createServer(app);
+const port=process.env.PORT || 5000;
+
+app.use(express.static(__dirname+'/public'));
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+/* socket.io setup */
+
+const io=require("socket.io")(server);
+var users={};
+
+io.on("connection",(socket)=>{
+    socket.on("new user joined",(username)=>{
+        users[socket.id]=username;
+        //console.log(users);
+        socket.broadcast.emit('user-connected',username);
+        io.emit("user-list",users);
+    });
+
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit('user-discon',user=users[socket.id]);
+        delete users[socket.id];
+        io.emit("user-list",users);
+    });
+
+    socket.on('message',(data)=>{
+        socket.broadcast.emit("message",{user: data.user,msg: data.msg});
+    });
+
+});
+
+
+
+server.listen(port, ()=>
+    {console.log("Server started at "+port);
+});
+
